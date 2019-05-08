8279eaed6fca9bafeb49b648fb0bb080607be543
John Blossom <jblossom.web@gmail.com>
2019-05-07 20:39:26 -0600

    stories for star_rating

--- src/components/star_rating/index.story.js
+++ src/components/star_rating/index.story.js
@@ -7,13 +7,26 @@ export const mockProps = {
   ...StarRating.defaultProps,
 };
 
+export const renderScene = knobProps => (knobs, props = knobProps) => (
+  <StarRating
+    rating={knobs.number('rating', props.rating)}
+    maxRating={knobs.number('maxRating', props.maxRating)}
+  />
+);
+
 export const scenarios = {
-  'default': (knobs, props = mockProps) => (
-    <StarRating
-      rating={knobs.number('rating', props.rating)}
-      maxRating={knobs.number('maxRating', props.maxRating)}
-    />
-  ),
+  'default': renderScene({
+    ...StarRating.defaultProps,
+  }),
+  'rating': renderScene({
+    ...StarRating.defaultProps,
+    rating: 4.20,
+  }),
+  'maxRating': renderScene({
+    ...StarRating.defaultProps,
+    rating: 4.20,
+    maxRating: 10,
+  }),
 };
 
 storyBuilder(scenarios, 'components/star_rating');
